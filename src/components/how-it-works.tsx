9a51598147a400862bb12b975027f3639f520d5f
Lincoln Madaraka <117019629+Lincoln-Madaraka@users.noreply.github.com>
2025-09-15 10:02:42 +0300

    how it works

--- src/components/how-it-works.tsx
+++ src/components/how-it-works.tsx
@@ -6,10 +6,10 @@ import WorkCard from "./work-card";
 
 const HowItWorks = () => {
   return (
-    <section id="how-it-works" className="py-20 bg-white md:h-screen dark:bg-gray-900">
+    <section id="how-it-works" className="py-20 bg-white md:h-screen">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <div className="text-center mb-16">
-          <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4 dark:text-white">
+          <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
             How It Works
           </h2>
           <p className="text-lg text-gray-600 max-w-2xl mx-auto dark:text-gray-400">
